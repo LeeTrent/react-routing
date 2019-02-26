0322d0f39f10af4acd1695d6c3de77ac6337f922
LeeTrent <Lee.Trent.1@gmail.com>
2019-02-26 09:11:41 -0600

    Uses Switch componenet to load a single route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -33,8 +34,11 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={ () => <h1>Casey</h1> } />
                 <Route path="/" render={ () => <h1>Pooh</h1> } /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
